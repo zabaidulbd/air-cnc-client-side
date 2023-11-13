36e85f40584d4780d1d9b7a0bb1f8e737ba4407e
zabaidulbd <zislam5622@gmail.com>
2023-11-13 19:11:39 +0600

    add login and signup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Categories from '../../components/Categories/Categories'
-// import Rooms from '../../components/Rooms/Rooms'
 
 const Home = () => {
     return (
         <>
             <Categories />
-            {/* <Rooms /> */}
         </>
     )
 }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,8 +3,6 @@ import Main from '../layouts/Main'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Login/Login'
 import SignUp from '../pages/SignUp/SignUp'
-// import RoomDetails from '../pages/RoomDetails/RoomDetails'
-// import PrivateRoute from './PrivateRoute'
 
 export const router = createBrowserRouter([
   {
@@ -14,16 +12,8 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
-      },
-      // {
-      //   path: '/room/:id',
-      //   element: (
-      //     <PrivateRoute>
-      //       <RoomDetails />
-      //     </PrivateRoute>
-      //   ),
-      // },
-    ],
+      }
+    ]
   },
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <SignUp /> },
