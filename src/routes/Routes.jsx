61bb3455a41334a9a2beb757a85a4787c783fc2d
zabaidulbd <zislam5622@gmail.com>
2023-11-15 15:32:48 +0600

    add calendar

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,8 @@ import Main from '../layouts/Main'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Login/Login'
 import SignUp from '../pages/SignUp/SignUp'
+import RoomDetails from '../pages/RoomDetails/RoomDetails'
+import PrivateRoute from './PrivateRoute'
 
 export const router = createBrowserRouter([
   {
@@ -12,8 +14,14 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
-      }
-    ]
+      },
+      {
+        path: '/room/:id',
+        element: (
+          <PrivateRoute><RoomDetails /></PrivateRoute>
+        ),
+      },
+    ],
   },
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <SignUp /> },
