126843d77139e54baf05ee949adf679473d31da9
zabaidulbd <zislam5622@gmail.com>
2023-11-25 18:40:47 +0600

    add host request modal

--- src/api/auth.js
+++ src/api/auth.js
@@ -16,23 +16,23 @@ export const saveUser = user => {
 }
 
 // become a host
-// export const becomeHost = email => {
-//     const currentUser = {
-//         role: 'host',
-//     }
+export const becomeHost = email => {
+    const currentUser = {
+        role: 'host',
+    }
 
-//     return fetch(`${import.meta.env.VITE_API_URL}/users/${email}`, {
-//         method: 'PUT',
-//         headers: {
-//             'content-type': 'application/json',
-//         },
-//         body: JSON.stringify(currentUser),
-//     }).then(res => res.json())
-// }
+    return fetch(`${import.meta.env.VITE_API_URL}/users/${email}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(currentUser),
+    }).then(res => res.json())
+}
 
 // Get role
-// export const getRole = async email => {
-//     const response = await fetch(`${import.meta.env.VITE_API_URL}/users/${email}`)
-//     const user = await response.json()
-//     return user?.role
-// }
+export const getRole = async email => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/users/${email}`)
+    const user = await response.json()
+    return user?.role
+}
